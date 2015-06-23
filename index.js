ed32a855eeaa76b7ab90819d045346e933208e93
ragingwind <ragingwind@gmail.com>
2015-06-22 20:53:57 -0700

    Using stream instead of callback

--- index.js
+++ index.js
@@ -1,53 +1,70 @@
 'use strict';
 
 var parser5 = require('parse5');
+var got = require('got-promise');
 var _ = require('lodash');
+var next = require('next-promise');
 
-function searchHref(attrs, search) {
-  if (!attrs) {
-    return null;
-  }
-
-  var href = attrs[_.findIndex(attrs, {name: 'href'})];
-
-  if (href && href.value.indexOf(search) !== -1) {
-    return href;
+function read(uri) {
+  if (_.startsWith(uri, 'file://')) {
+    return fs.createReadStream(uri);
+  } else if (_.startsWith(uri, 'http')) {
+    return got(uri);
   } else {
-    return null;
+    var Stream = require('stream');
+    var stream = new Stream();
+    stream.pipe = function(dest) {
+      dest.write(uri)
+      return dest;
+    }
+    return stream;
   }
-};
+}
 
-function extractLink(html) {
-		var link = [];
-		var parser = new parser5.SimpleApiParser({
-			startTag: function(name, attrs, selfClosing) {
-				if (name === 'link') {
-          var href = searchHref(attrs, 'fonts.googleapis.com');
-          if (href) {
-            link.push(href);
-          }
+function parseLink(html) {
+  var link = [];
+  var parser = new parser5.SimpleApiParser({
+    startTag: function(name, attrs, selfClosing) {
+      if (name === 'link' && attrs) {
+        var href = attrs[_.findIndex(attrs, {name: 'href'})];
+
+        if (href && href.value.indexOf('fonts.googleapis.com') !== -1) {
+          link.push(href);
         }
-			}
-		});
+      }
+    }
+  });
 
-    parser.parse(html);
+  parser.parse(html);
 
-    return link;
-};
+  return link;
+}
 
-function importer(html) {
-	var ret = null;
-	try {
-    var link = extractLink(html);
-  } catch (e) {
-		console.error(e);
-	}
-};
 
-module.exports = function (opts) {
-	opts = opts || {};
+function parse() {
+  var Transform = require('stream').Transform;
+  var parser = new Transform({objectMode: true});
+  parser._transform = function(data, encoding, done) {
+    var link = parseLink(data);
+    this.push(link);
+    done();
+  };
+  return parser;
+}
 
-	if (opts.html) {
-		importer(opts.html);
-	}
+function download() {
+  var Transform = require('stream').Transform;
+  var parser = new Transform({objectMode: true});
+  parser._transform = function(data, encoding, done) {
+    console.log(data);
+    done();
+  };
+  return parser;
+}
+
+module.exports = {
+  get: function(uri) {
+    read(uri).pipe(parse())
+             .pipe(download());
+  }
 };
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 'use strict';
 var assert = require('assert');
-var googleFontImporter = require('./');
+var gfgot = require('./');
 
 var html = '<html><head><link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,500italic,700,700italic"></head></html>';
 
 it('Simple parse', function () {
-	googleFontImporter({html:html});
+	gfgot.get(html);
 	assert(true);
 });
